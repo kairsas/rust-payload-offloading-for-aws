2509d85c1970e509a95b6403b7bffcda9f2da11b
Mindaugas Kairys <kairius@gmail.com>
2025-09-17 15:53:51 +0300

    Allow unsafe in tests (for ctor)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-#![forbid(unsafe_code)]
+#![cfg_attr(not(test), forbid(unsafe_code))]
+#![cfg_attr(test, allow(unsafe_code))]
 #![deny(clippy::panic, clippy::expect_used, clippy::unwrap_used)]
 #![cfg_attr(feature = "sqs", cfg_attr(feature = "sns", doc = include_str!("../README.md")))]
 
